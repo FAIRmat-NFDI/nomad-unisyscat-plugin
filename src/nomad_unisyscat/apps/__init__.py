fe2020134e5eb4d86db969f1710f547aa3f40111
Julia Schumann <32182254+schumannj@users.noreply.github.com>
2024-06-07 10:35:36 +0000

    add one widget

--- src/nomad_unisyscat/apps/__init__.py
+++ src/nomad_unisyscat/apps/__init__.py
@@ -1,6 +1,12 @@
 from nomad.config.models.plugins import AppEntryPoint
-from nomad.config.models.ui import App, Column, Columns, FilterMenu, FilterMenus
-
+from nomad.config.models.ui import (
+    App,
+    Column,
+    Columns,
+    FilterMenu,
+    FilterMenus,
+    Filters,
+)
 
 myapp = AppEntryPoint(
     name='UniSysCatApp',
@@ -12,11 +18,11 @@ myapp = AppEntryPoint(
         columns=Columns(
             selected=[
                 'entry_id',
-                'data.method#nomad_unisyscat.schema_packages.mypackage.NRVSpectroscopy'
+                'data.method#nomad_unisyscat.schema_packages.mypackage'
             ],
             options={
                 'entry_id': Column(),
-                'data.method#nomad_unisyscat.schema_packages.mypackage.NRVSpectroscopy': Column()
+                'data.method#nomad_unisyscat.schema_packages.mypackage': Column()
             },
         ),
 
@@ -28,5 +34,49 @@ myapp = AppEntryPoint(
         filters=Filters(
             include=['*#nomad_unisyscat.schema_packages.mypackage'],
         ),
+        dashboard={
+            'widgets': [
+                {
+                    'type': 'terms',
+                    'scale': 'linear',
+                    'quantity': 'data.method#nomad_unisyscat.schema_packages.mypackage',
+                    'layout': {
+                        'xxl': {
+                            'minH': 3,
+                            'minW': 3,
+                            'h': 9,
+                            'w': 6,
+                            'y': 0,
+                            'x': 10,
+                        },
+                        'xl': {
+                            'minH': 3,
+                            'minW': 3,
+                            'h': 9,
+                            'w': 6,
+                            'y': 0,
+                            'x': 10,  #'.inf',
+                        },
+                        'lg': {'minH': 3, 'minW': 3, 'h': 4, 'w': 5, 'y': 0, 'x': 0},
+                        'md': {
+                            'minH': 3,
+                            'minW': 3,
+                            'h': 9,
+                            'w': 6,
+                            'y': 0,
+                            'x': 10,  #'.inf',
+                        },
+                        'sm': {
+                            'minH': 3,
+                            'minW': 3,
+                            'h': 9,
+                            'w': 6,
+                            'y': 0,
+                            'x': 10,  #'.inf',
+                        },
+                    },
+                },
+            ],
+        }
     ),
 )
